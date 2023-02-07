dd29880d48ce7fd6ea4ac4c732c6865a2a30025d
Royce Lee <445081882@qq.com>
2023-02-07 14:20:43 +0800

    init
    init

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(animal),
+            temperature=0,
+            max_tokens=128,
+        )
+        for choice in response.choices:
+            print(choice)
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+def generate_prompt(animal):
+    # return """假设你是带货直播里的观众，你想问这个问题：这件衣服有多大？对方可能会回答："""
+    return animal
+    
+
+
+# def generate_prompt(animal):
+#     return """Suggest three names for an animal that is a superhero.
+
+# Animal: Cat
+# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+# Animal: Dog
+# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+# Animal: {}
+# Names:""".format(
+#         animal.capitalize()
+#     )
